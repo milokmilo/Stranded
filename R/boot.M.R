8445782ceba67d0ef3b7a6c17ed2cca893383f5a
milokmilo <milokmilo@gmail.com>
2015-12-03 14:10:04 +0100

    Updating boot.M for using Siler parameters and Siler life table

--- R/boot.M.R
+++ R/boot.M.R
@@ -9,19 +9,25 @@
 #' boot.M()
 #' 
 
-boot.M <- function(data, boot.n){
+boot.M <- function(data, SilerPar, boot.n, lifeTab){
+  a1 <- SilerPar[1]
+  b1 <- SilerPar[2]
+  a2 <- SilerPar[3]
+  a3 <- SilerPar[4]
+  b3 <- SilerPar[5]
   boots <- boot.n
   M <- numeric()
   nr <- nrow(data)
   n <- round(runif(boots, 2, nr))  
+  ag <- data$age
   for(i in 1:boots){
     Bo <- data[-n[i],]
-    par <- optim(c(a1,b1,a2,a3,b3),fit.Siler2, data=Bo,
-                 control=list(fnscale=-1,maxit=100))
-    M <- rbind(M, par$par[1]*exp(-par$par[2]*ag)+par$par[3]+par$par[4]*exp(par$par[5]*ag))
+    par <- SilerMod(c(a1,b1,a2,a3,b3),data=Bo)
+    M <- rbind(M, par$par[1]*exp(-par$par[2]*ag) +
+                 par$par[3] + par$par[4]*exp(par$par[5]*ag))
   }
-  Mplot <- t(as.data.frame(M))
-  rownames(Mplot) <- (1:nrow(Mplot))-1
-  Mplot <- t(Mplot)
-  return(Mplot)
+  bootM <- t(as.data.frame(M))
+  rownames(bootM) <- (1:nrow(bootM))-1
+  bootM[bootM<0] <- lifeTab[1,"qx"]
+  return(bootM)
 }
